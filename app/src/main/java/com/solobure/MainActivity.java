bf4e45c2cfcb37fec2f139571517ae0cfc402839
arith-y-ono <y.ono@arith-metic.jp>
2017-01-13 14:52:28 +0900

    web viewを追加した

--- app/src/main/java/com/solobure/MainActivity.java
+++ app/src/main/java/com/solobure/MainActivity.java
@@ -2,12 +2,24 @@ package com.solobure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+// web viewをインポート
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String accessUrl = "35.162.151.33";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView solobura =  (WebView)findViewById(R.id.webView1);
+        //標準ブラウザをキャンセル
+        solobura.setWebViewClient(new WebViewClient());
+        //アプリ起動時に読み込むURL
+        solobura.loadUrl(accessUrl);
+
     }
 }
